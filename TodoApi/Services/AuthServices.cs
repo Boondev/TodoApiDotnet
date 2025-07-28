86289b2ffc7382936322d6b64e70f5869aca91ea
boon <boon@mkverse.technology>
2025-07-28 11:15:24 +0800

    init database

--- TodoApi/Services/AuthServices.cs
+++ TodoApi/Services/AuthServices.cs
@@ -71,7 +71,7 @@ public class AuthService(TodoDbContext _context, IMapper _mapper, IConfiguration
         var token = new JwtSecurityToken(
             issuer: _configuration["JWT:ValidIssuer"],
             audience: _configuration["JWT:ValidAudience"],
-            expires: DateTime.Now.AddHours(3),
+            expires: DateTime.Now.AddDays(7),
             claims: claims,
             signingCredentials: new SigningCredentials(
                 authSigningKey,
